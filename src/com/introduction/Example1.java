bc69a17c61d09815b801284b94e93dea0e4aacf6
MidhunBalan <midhun.balan@anywhere.co>
2021-03-31 08:08:43 +0530

    java 9 features

---
+++ src/com/introduction/Example1.java
@@ -0,0 +1,7 @@
+package com.introduction;
+
+public class Example1 {
+    public static void main(String arg[]){
+        System.out.println("current process id:"+ProcessHandle.current().pid());
+    }
+}
